119971fab8db024a91c365d36de99c353651b140
Gabor Nemethi <nemethigabo@gmail.com>
2020-04-17 20:11:22 +0200

    Add SzemelyNevValidator

---
+++ comp-szemely/src/main/java/nemethi/szemely/validation/SzemelyNevValidator.java
@@ -0,0 +1,45 @@
+package nemethi.szemely.validation;
+
+import validation.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SzemelyNevValidator implements Validator<String> {
+
+    private static final String NEV_PATTERN = "([a-zA-ZäáéíóöőúüűÄÁÉÍÓÖŐÚÜŰ./'\\-]+) ( ?([a-zA-ZäáéíóöőúüűÄÁÉÍÓÖŐÚÜŰ./'\\-]+))+";
+    private static final String INVALID_NAME_MESSAGE = "Érvénytelen %s név";
+    private static final String TOO_LONG_NAME_MESSAGE = "Túl hosszú %s név";
+    private final String nameType;
+
+    public SzemelyNevValidator(String nameType) {
+        this.nameType = Objects.requireNonNull(nameType, "nameType");
+    }
+
+    @Override
+    public List<String> validate(String name) {
+        List<String> errors = new ArrayList<>();
+        name = removeDrPrefixAndSuffix(name);
+        if (!name.matches(NEV_PATTERN)) {
+            errors.add(String.format(INVALID_NAME_MESSAGE, nameType));
+        }
+        if (name.length() > 80) {
+            errors.add(String.format(TOO_LONG_NAME_MESSAGE, nameType));
+        }
+        return errors;
+    }
+
+    private String removeDrPrefixAndSuffix(String name) {
+        name = name.trim();
+        if (name.regionMatches(true, 0, "dr.", 0, 3)) {
+            name = name.substring(3);
+        }
+
+        if (name.regionMatches(true, name.length() - 3, "dr.", 0, 3)) {
+            name = name.substring(0, name.length() - 3);
+        }
+
+        return name.trim();
+    }
+}
---
+++ comp-szemely/src/test/java/nemethi/szemely/validation/SzemelyNevValidatorTest.java
@@ -0,0 +1,119 @@
+package nemethi.szemely.validation;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SzemelyNevValidatorTest {
+
+    private static final String INVALID_NAME_MESSAGE = "Érvénytelen viselt név";
+    private static final String TOO_LONG_NAME_MESSAGE = "Túl hosszú viselt név";
+    private static final String TOO_LONG_NAME = repeat("A", 40) + " " + repeat("B", 40);
+    private static final String NAME_WITH_SPECIAL_CHARS = "Kovács-Szäbó D. Ödön/Péter L'Äuren";
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private SzemelyNevValidator validator;
+
+    @Before
+    public void setUp() {
+        validator = new SzemelyNevValidator("viselt");
+    }
+
+    @Test
+    public void constructorThrowsExceptionWhenNameTypeIsNull() {
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("nameType");
+        new SzemelyNevValidator(null);
+    }
+
+    @Test
+    public void validateReturnsEmptyListWhenNameIsValid() {
+        // when
+        List<String> errors = validator.validate("Kovács István");
+
+        // then
+        assertThat(errors).isEmpty();
+    }
+
+    @Test
+    public void validateReturnsErrorListWhenNameHasOnlyOnePart() {
+        // when
+        List<String> errors = validator.validate("Kovács");
+
+        // then
+        assertThat(errors).hasSize(1);
+        assertThat(errors).containsExactly(INVALID_NAME_MESSAGE);
+    }
+
+    @Test
+    public void validateReturnsErrorListWhenNameIsTooLong() {
+        // when
+        List<String> errors = validator.validate(TOO_LONG_NAME);
+
+        // then
+        assertThat(errors).hasSize(1);
+        assertThat(errors).containsExactly(TOO_LONG_NAME_MESSAGE);
+    }
+
+    @Test
+    public void validateReturnsEmptyListOnValidNameWithSpecialCharacters() {
+        // when
+        List<String> errors = validator.validate(NAME_WITH_SPECIAL_CHARS);
+
+        // then
+        assertThat(errors).isEmpty();
+    }
+
+    @Test
+    public void validateDoesNotConsiderDrPrefixInName() {
+        // when
+        List<String> errors = validator.validate("dr. Kovács István");
+
+        // then
+        assertThat(errors).isEmpty();
+
+        // when
+        errors = validator.validate("Dr. Kovács");
+
+        // then
+        assertThat(errors).hasSize(1);
+        assertThat(errors).containsExactly(INVALID_NAME_MESSAGE);
+    }
+
+    @Test
+    public void validateDoesNotConsiderDrSuffixInName() {
+        // when
+        List<String> errors = validator.validate("Kovács István dr.");
+
+        // then
+        assertThat(errors).isEmpty();
+
+        // when
+        errors = validator.validate("Kovács Dr.");
+
+        // then
+        assertThat(errors).hasSize(1);
+        assertThat(errors).containsExactly(INVALID_NAME_MESSAGE);
+    }
+
+    @Test
+    public void validateDoesNotConsiderLeadingAndTrailingSpaces() {
+        // when
+        List<String> errors = validator.validate(" Kovács István ");
+
+        // then
+        assertThat(errors).isEmpty();
+    }
+
+    private static String repeat(String string, int times) {
+        return String.join("", Collections.nCopies(times, string));
+    }
+}
